a549a36a4f13f7c67d0d9dfd0153c38e1476775f
Ashlee Cannon <cannonash929@gmail.com>
2018-07-09 16:14:30 -0500

    update to files

--- js/app.js
+++ js/app.js
@@ -35,7 +35,7 @@ const deck = document.querySelector('.deck');
 
 function createCards(){
     for(let card of cards){
-        let singleCard = document.createElement('li');
+        const singleCard = document.createElement('li');
         singleCard.innerHTML=`<li class="card hide"><i class="fa ${card}"></i></li>`; 
         deck.appendChild(singleCard); 
     }
@@ -51,7 +51,17 @@ function createCards(){
 
      }); 
 
+ 
 
+
+
+
+const openedCards = []; 
+
+function matchCards(){
+   
+    
+} matchCards(); 
 
 
 /*
